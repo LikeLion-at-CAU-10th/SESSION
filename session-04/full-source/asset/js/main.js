b7f8bc6975b621564caa9aabdd7c518f8f484929
youngkwon02 <youngkwon02@naver.com>
2022-04-21 00:59:03 +0900

    refactor: session04/overall script and static files

--- session-04/full-source/asset/js/main.js
+++ session-04/full-source/asset/js/main.js
@@ -1,3 +1,5 @@
+import { fetchGET, fetchPOST } from "./dataFetching.js";
+
 const SERVER_HOST =
   "https://asia-northeast3-likelion-js-practice.cloudfunctions.net/api";
 
@@ -12,3 +14,11 @@ async function getProfileData(name) {
   document.querySelector("#fetch-mbti").innerHTML = profile.mbti;
   document.querySelector("#fetch-github").innerHTML = profile.github;
 }
+
+window.onload = () => {
+  // 웹 페이지의 모든 element(HTML, CSS)가 화면에 띄워지면 실행됩니다.
+  console.log("page load!");
+
+  // 프로필 정보를 서버에서 받아와서 업데이트 합니다.
+  getProfileData("김영권");
+};
