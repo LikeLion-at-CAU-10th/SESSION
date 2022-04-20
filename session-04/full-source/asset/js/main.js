c7d6846db0d59e3e703ea27113ac7debad1b69ba
youngkwon02 <youngkwon02@naver.com>
2022-04-20 14:47:01 +0900

    feat: getProfileData func for data fetching

---
+++ session-04/full-source/asset/js/main.js
@@ -0,0 +1,14 @@
+const SERVER_HOST =
+  "https://asia-northeast3-likelion-js-practice.cloudfunctions.net/api";
+
+const getProfileData = async (name) => {
+  const path = "/member";
+
+  const res = await fetchGET(SERVER_HOST, path, { name });
+  const resData = res.data;
+  const profile = resData.profile;
+
+  document.querySelector("#fetch-name").innerHTML = profile.name;
+  document.querySelector("#fetch-mbti").innerHTML = profile.mbti;
+  document.querySelector("#fetch-github").innerHTML = profile.github;
+};
