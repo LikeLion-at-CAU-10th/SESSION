c988b6ed841d6a2e4e42ff57cbf495e4a838cac2
youngkwon02 <youngkwon02@naver.com>
2022-04-21 01:13:28 +0900

    feat: session04/getFootprint function

--- session-04/full-source/asset/js/main.js
+++ session-04/full-source/asset/js/main.js
@@ -3,6 +3,8 @@ import { fetchGET, fetchPOST } from "./dataFetching.js";
 const SERVER_HOST =
   "https://asia-northeast3-likelion-js-practice.cloudfunctions.net/api";
 
+const NAME = "김영권";
+
 async function getProfileData(name) {
   const path = "/member";
 
@@ -15,10 +17,27 @@ async function getProfileData(name) {
   document.querySelector("#fetch-github").innerHTML = profile.github;
 }
 
+async function getFootprint(name) {
+  const path = "/message";
+
+  const res = await fetchGET(SERVER_HOST, path, { name });
+  const resData = res.data;
+  const messages = resData.messages;
+  const footprintBoard = document.querySelector(".board");
+  footprintBoard.innerHTML = "";
+
+  messages.forEach((m) => {
+    footprintBoard.innerHTML += `<div class="board-row">${m}</div>`;
+  });
+}
+
 window.onload = () => {
   // 웹 페이지의 모든 element(HTML, CSS)가 화면에 띄워지면 실행됩니다.
   console.log("page load!");
 
-  // 프로필 정보를 서버에서 받아와서 업데이트 합니다.
-  getProfileData("김영권");
+  // 프로필 정보를 서버에서 받아와서 DOM을 업데이트 합니다.
+  getProfileData(NAME);
+
+  // 메시지를 서버에서 받아와서 DOM을 업데이트 합니다.
+  getFootprint(NAME);
 };
