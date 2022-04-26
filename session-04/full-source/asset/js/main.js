8c2199e09aa870f949233f2936ecf3b0a2df9597
youngkwon02 <youngkwon02@naver.com>
2022-04-27 00:39:32 +0900

    feat: main.js server response error handling

--- session-04/full-source/asset/js/main.js
+++ session-04/full-source/asset/js/main.js
@@ -9,18 +9,30 @@ async function getProfileData(name) {
   const path = "/member";
 
   const res = await fetchGET(SERVER_HOST, path, { name });
+
+  if (res.status !== 200) {
+    return false;
+  }
+
   const resData = res.data;
   const profile = resData.profile;
 
   document.querySelector("#fetch-name").innerHTML = profile.name;
   document.querySelector("#fetch-mbti").innerHTML = profile.mbti;
   document.querySelector("#fetch-github").innerHTML = profile.github;
+
+  return true;
 }
 
 async function getFootprint(name) {
-  const path = "/message";
+  const path = "/footprint";
 
   const res = await fetchGET(SERVER_HOST, path, { name });
+
+  if (res.status !== 200) {
+    return false;
+  }
+
   const resData = res.data;
   const messages = resData.messages;
   const footprintBoard = document.querySelector(".board");
@@ -29,10 +41,12 @@ async function getFootprint(name) {
   messages.forEach((m) => {
     footprintBoard.innerHTML += `<div class="board-row">${m}</div>`;
   });
+
+  return true;
 }
 
 async function sendFootprint() {
-  const path = "/message";
+  const path = "/footprint";
 
   const message = document.querySelector(".message-content").value;
   const sender = document.querySelector(".message-sender").value;
@@ -44,11 +58,25 @@ async function sendFootprint() {
 
   if (res.status === 200) {
     alert("메시지를 성공적으로 전송하였습니다.");
+    location.reload();
+  } else {
+    alert("메시지 전송에 실패하였습니다.");
   }
 }
 
-// 프로필 정보를 서버에서 받아와서 DOM을 업데이트 합니다.
-getProfileData(NAME);
+window.onload = function () {
+  // 프로필 정보를 서버에서 받아와서 DOM을 업데이트 합니다.
+  const isGetProfileSuccess = getProfileData(NAME);
 
-// 메시지를 서버에서 받아와서 DOM을 업데이트 합니다.
-getFootprint(NAME);
+  // 메시지를 서버에서 받아와서 DOM을 업데이트 합니다.
+  const isGetFootprintSuccess = getFootprint(NAME);
+
+  if (!isGetProfileSuccess || !isGetFootprintSuccess) {
+    alert("데이터 로딩에 실패하였습니다.");
+  }
+
+  const sendButton = document.querySelector(".send-btn");
+  sendButton.addEventListener("click", () => {
+    sendFootprint();
+  });
+};
