81c82a6b6bb478f1fe6da2244911704b911045d9
myeongjunkim <ilikerunning@naver.com>
2022-04-30 04:16:28 +0900

    session 05 ready

--- session-05/get_json.py
+++ session-05/get_json.py
@@ -8,39 +8,6 @@
 # myvenv 도 할까??
 # 간단 기본 문법(주요 자료형 리스트, 딕셔너리) => 내장 함수
 
-# 데이터 구조
-list_a = [1,2,3,4]
-list_a.append(2)
-
-dic_a = {}
-dic_a["명준"] = "바보"
-
-
-# 반복 조건문
-# for item in list_a:
-#     print(item)
-
-# for i in range(5):
-#     if i == 4:
-#         # print(i)
-
-
-# 문자열 파싱
-str_a = "a, b, c, d, e"
-ret = str_a.split(",")
-
-str_num = "1 2 3 4 5"
-ret2 = str_num.split()
-
-# 문자열과 인트형 차이!!
-numlist = []
-for item in ret2:
-    numlist.append(int(item))
-
-
-numlist = list(map(int, str_num.split()))
-# print(numlist)
-
 
 
 ###########################################################
@@ -54,7 +21,8 @@ content_json = json.loads(content.content)
 content_json = json.dumps(content_json, sort_keys=False, indent=4)
 
 
-url = "https://data.gm.go.kr/openapi/Ducklbrd?Key=1ed3d9acf9a440bdb4453b5122f5afed""&Type=json"
+url = "https://data.gm.go.kr/openapi/Ducklbrd?Key=1ed3d9acf9a440bdb4453b5122f5afed&Type=json"
+
 data = requests.get(url) 
 
 data_json = json.loads(data.content)
