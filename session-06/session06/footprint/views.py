6cdb06f823504b0feef5ed16de0d91d1caa90864
youngkwon02 <youngkwon02@naver.com>
2022-05-11 20:46:33 +0900

    feat: session-06/footprintGET API function

--- session-06/session06/footprint/views.py
+++ session-06/session06/footprint/views.py
@@ -1,8 +1,20 @@
-from django.shortcuts import render
+from footprint.models import Footprint
+from django.http import JsonResponse
 
 
 def footprint_GET(request):
-    pass
+    footprints = Footprint.objects.filter(receiver="김영권").order_by('-sent_at')
+    messages = []
+    for i in range(len(footprints)):
+        messages.append(footprints[i].message)
+
+    return JsonResponse({
+        "status": 200,
+        "message": "Footprint 조회 성공",
+        "data": {
+            "messages": messages
+        }
+    })
 
 
 def footprint_POST(request):
