cfd80ae651ce0ec4c6557d2d21de64cc6aed13b6
youngkwon02 <youngkwon02@naver.com>
2022-05-11 20:17:43 +0900

    chore: define Footprint DB model

--- session-06/session06/footprint/models.py
+++ session-06/session06/footprint/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+
+class Footprint(models.Model):
+    footprint_id = models.AutoField(primary_key=True)
+    receiver = models.TextField(null=False)
+    message = models.TextField(null=False)
+    sent_at = models.DateTimeField(auto_now_add=True, blank=True)
