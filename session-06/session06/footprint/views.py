87b1fc708acd6713c29d465ad3603b5e901d0ff5
youngkwon02 <youngkwon02@naver.com>
2022-05-15 16:19:50 +0900

    fix: session-06/footprint_POST body key value

--- session-06/session06/footprint/views.py
+++ session-06/session06/footprint/views.py
@@ -22,8 +22,8 @@ def footprint_POST(request):
     body_unicode = request.body.decode('utf-8')
     body = json.loads(body_unicode)
     Footprint.objects.create(
-        message=body['message'], receiver=body['receiver'])
-
+        message=body['content'], receiver=body['receiverName'])
+    
     return JsonResponse({
         'status': 200,
         'message': '메시지 전송 성공'
