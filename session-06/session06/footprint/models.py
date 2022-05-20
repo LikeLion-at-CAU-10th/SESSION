d319b3b69f12a83c5eb1cb101534b4741cc36b98
youngkwon02 <youngkwon02@naver.com>
2022-05-20 15:23:18 +0900

    comment: session-06/footprint.models

--- session-06/session06/footprint/models.py
+++ session-06/session06/footprint/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 class Footprint(models.Model):
-    footprint_id = models.AutoField(primary_key=True)
-    receiver = models.TextField(null=False)
-    message = models.TextField(null=False)
-    sent_at = models.DateTimeField(auto_now_add=True, blank=True)
+    footprint_id = models.AutoField(primary_key=True)       # AutoField로 설정해서 footprint_id는 자동으로 1씩 증가된 값이 부여됩니다
+    receiver = models.TextField(null=False)                 # receiver는 텍스트입니다
+    message = models.TextField(null=False)                  # message는 텍스트입니다
+    sent_at = models.DateTimeField(auto_now_add=True, blank=True)   # 메시지 전송 시각은, 자동으로 데이터가 생성된 시간으로 값을 부여합니다
